0eb291cf9e8dfc406fdabcc9633810d00cce9723
hpachecot <hpachecot@unicesar.edu.co>
2022-11-24 16:11:12 -0500

    .

--- Presentacion/FrmConsultaClientes.Designer.cs
+++ Presentacion/FrmConsultaClientes.Designer.cs
@@ -28,33 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.GrillaClientes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GrillaClientes)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // GrillaClientes
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(803, 451);
-            this.dataGridView1.TabIndex = 0;
+            this.GrillaClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GrillaClientes.Location = new System.Drawing.Point(0, 0);
+            this.GrillaClientes.Name = "GrillaClientes";
+            this.GrillaClientes.Size = new System.Drawing.Size(803, 451);
+            this.GrillaClientes.TabIndex = 0;
             // 
-            // ConsultaClientes
+            // FrmConsultaClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "ConsultaClientes";
+            this.Controls.Add(this.GrillaClientes);
+            this.Name = "FrmConsultaClientes";
             this.Text = "ConsultaClientes";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrillaClientes)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView GrillaClientes;
     }
 }
